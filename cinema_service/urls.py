ee3645949f63ec637fef67c39ae9b869e22cdbe7
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-30 17:55:33 +0300

    Delete django-debug-toolbar

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,8 +1,7 @@
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/cinema/", include("cinema.urls", namespace="cinema"))
-] + debug_toolbar_urls()
+]
